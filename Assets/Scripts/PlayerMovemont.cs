144f362946b5921871de98a911efdaef997c8c2b
Danijel Stracenski <danijels222@gmail.com>
2021-10-19 12:27:32 +0200

    Jump animation, ground taken in consideration

--- Assets/Scripts/PlayerMovemont.cs
+++ Assets/Scripts/PlayerMovemont.cs
@@ -1,26 +1,33 @@
 using UnityEngine;
 
 [RequireComponent(typeof (Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovemont : MonoBehaviour
 {
-    private Rigidbody2D body;
+    private Rigidbody2D rigidbody;
+    private Animator animator;
     [SerializeField] private float speed;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        checkHorizontalShift();
-        checkJump();
+        checkHorizontalShiftInput();
+        checkJumpInput();
     }
 
-    private void checkHorizontalShift() {
+    private void checkHorizontalShiftInput() {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        rigidbody.velocity = new Vector2(horizontalInput * speed, rigidbody.velocity.y);
+
+        #region animation and sprites
         checkHorizontalSpriteFlip(horizontalInput);
+        animator.SetBool("isRunning", horizontalInput != 0);
+        #endregion
     }
 
     private void checkHorizontalSpriteFlip(float horizontalInput) {
@@ -30,11 +37,22 @@ public class PlayerMovemont : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
     }
 
-    private void checkJump() {
-        if (Input.GetKey(KeyCode.Space))
+    private void checkJumpInput() {
+        if (Input.GetKey(KeyCode.Space) && animator.GetBool("isGrounded"))
         {
-            body.velocity = new Vector2(body.velocity.x, speed / 2);
+            jump();
         }
+    }
+
+    private void jump() {
+        animator.SetBool("isGrounded", false);
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, speed / 2);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+            animator.SetBool("isGrounded", true);
     }
 
 }
